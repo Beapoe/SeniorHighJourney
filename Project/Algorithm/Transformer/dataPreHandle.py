4565538509a978db15e6f018caa788a1b1e6d453
Beapoe <2713906889@qq.com>
2025-08-31 00:04:13 +0800

    update

--- Project/Algorithm/Transformer/dataPreHandle.py
+++ Project/Algorithm/Transformer/dataPreHandle.py
@@ -1,16 +1,16 @@
 import torch
 import os
 
-if "ADreamofRedMansions.txt" not in os.getcwd(): 
-    os.chdir("./Project/Algorithm/Transformer")
 with open("ADreamofRedMansions.txt","+r",encoding="utf-8") as file:
     originTxt = file.read()
 
 chars = sorted(list(set(originTxt)))
 str2token = {idx:s for s,idx in enumerate(chars)}
-str2token.popitem()
 token2str = {s:idx for s,idx in enumerate(chars)}
-token2str.popitem()
+print(str2token)
 
 encode = lambda str:[str2token[c] for c in str]
 decode = lambda list:"".join([token2str[idx] for idx in list])
+
+data = torch.tensor(encode(originTxt),dtype=torch.long)
+print(data)
